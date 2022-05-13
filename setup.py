71243c70118aeae6a2ce8f8f0864f6000d687265
Austin David Brown <modparadigm@gmail.com>
2022-05-13 01:28:14 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from setuptools import setup, find_packages
 
 setup(
   name="cmhi", 
-  version="0.3",
+  version="0.4",
   author="Austin Brown",
   author_email="brow5079@umn.edu",
-  description="Centered Metropolis-Hastings independence algorithm for Bayesian logistic regression",
+  description="Centered Metropolis-Hastings independence sampler for Bayesian logistic regression",
   url = "https://github.com/austindavidbrown/Centered-Metropolis-Hastings",
   packages=["cmhi"],
   install_requires=["torch >= 1.9.1"],
